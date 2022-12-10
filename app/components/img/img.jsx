b76647afde507a1420584d167637a5d4ae5a1297
cristian cobo <ccobo1405@gmail.com>
2022-12-10 04:52:36 -0500

    da

---
+++ app/components/img/img.jsx
@@ -0,0 +1,39 @@
+import { Form } from "@remix-run/react";
+import fs from "fs";
+import { env } from "process";
+const path = require('path');
+
+export default function FormImg(e) {
+
+    function ImgOnchage(e) {
+        console.log(env);
+        // fs.writeFile('/text.md', 'archivo creado', (err) => {
+        //     if (err) throw err;
+        //     console.log('se creo bien');
+        // })
+        // const file = e.target.files[0]
+        // const formData = new FormData()
+        // formData.append('myFile', file, "prueba1");
+        // const f = formData.myFile
+        // console.log(formData);
+        // const path = '/public/images/' + "prueba1";
+        // f.mv(path, (error) => {
+        //     if (error) {
+        //         console.error(error);
+        //         return;
+        //     }
+        //     return 1;
+        // });
+
+
+
+
+    }
+    return (
+        <Form method="post" encType="multipart/form-data">
+            <label htmlFor="avatar-input">Avatar</label>
+            <input id="avatar-input" type="file" name="avatar" onChange={(e) => ImgOnchage(e)} />
+            <button>Upload</button>
+        </Form>
+    )
+}
